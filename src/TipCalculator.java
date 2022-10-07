259db9374b8756cd29598f9fa3555f21b3088ca7
Alexandre Arturi <alexandrea8@nycstudents.net>
2022-10-07 11:58:48 -0400

    10/7

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+    private int numPeople;
+    private double tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numPeople, double tipPercentage, double totalBillBeforeTip){
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage/100;
+        this.totalBillBeforeTip = totalBillBeforeTip;
+    }
+
+    public double getTotalBillBeforeTip(){
+        return totalBillBeforeTip;
+    }
+
+    public double getTipPercentage(){
+        return tipPercentage;
+    }
+
+    public void addMeal(double cost){
+        totalBillBeforeTip += cost;
+    }
+
+    public double tipAmount(){
+        return tipPercentage * totalBillBeforeTip;
+    }
+
+    public double totalBill(){
+        return totalBillBeforeTip + tipAmount();
+    }
+
+    public double perPersonCostBeforeTip(){
+        return totalBillBeforeTip/numPeople;
+    }
+
+    public double perPersonTipAmount(){
+        return tipAmount()/numPeople;
+    }
+
+    public double perPersonTotalCost(){
+        return perPersonTipAmount() + perPersonCostBeforeTip();
+    }
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class TipCalculatorRunner {
+import java.util.Scanner;
+
+public class TipCalculatorRunner {
+    public static void main (String [] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Input the number of people in the party: ");
+        int people = scan.nextInt();
+        System.out.println("Tip percentage(do not use percent symbol): ");
+        double tip = scan.nextDouble();
+        System.out.println("Input bill cost: ");
+        double billBeforeTip = scan.nextDouble();
+
+        TipCalculator bill = new TipCalculator(people, tip, billBeforeTip);
+
+        //producing output to user. Also, rounding all numbers to 2 decimal places
+        System.out.println("The total bill is $" + String.format("%.2f", bill.totalBill()));
+        System.out.println("The total tip is $" + String.format("%.2f", bill.tipAmount()));
+        System.out.println("The tip per person is $" + String.format("%.2f", bill.perPersonTipAmount()));
+        System.out.println("The total per person is $" + String.format("%.2f", bill.perPersonTotalCost()));
+
+    }
 }
